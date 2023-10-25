885b3041d4f7b1ee98dd913190c15198e1cd5e3b
Yujiro Shito <s.yujiro613@gmal.com>
2023-10-26 05:36:18 +0900

    New Commit test.bash

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	ret=1
+}
+
+ret=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$ret" = 0 ] && echo OK
+exit $ret
