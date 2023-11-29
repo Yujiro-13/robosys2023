783cfd3655b9d7c3541a87b09241dc78fabbd929
Yujiro Shito <s.yujiro613@gmal.com>
2023-11-29 11:30:42 +0900

    test.bashにcalculatorのテストを追加

--- test.bash
+++ test.bash
@@ -17,6 +17,7 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(seq -5 | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "Error" ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
@@ -35,6 +36,37 @@ out=$(echo あ | ./plus)
 out=$(echo | ./plus) #空文字
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "Error" ] || ng ${LINENO}
+
+### CALCULATOR TEST ###
+out=$(echo "1 + 1" | ./calculator)
+[ "${out}" = "Result: 2" ] || ng ${LINENO}
+
+out=$(echo "7 + 3" | ./calculator)
+[ "${out}" = "Result: 10" ] || ng ${LINENO}
+
+out=$(echo "5 - 4" | ./calculator)
+[ "${out}" = "Result: 1" ] || ng ${LINENO}
+
+out=$(echo "6 * 8" | ./calculator)
+[ "${out}" = "Result: 48" ] || ng ${LINENO}
+
+out=$(echo "9 / 2" | ./calculator)
+[ "${out}" = "Result: 4.5" ] || ng ${LINENO}
+
+out=$(echo "a + 1" | ./calculator)
+[ "${out}" = "Input Error: 不明な入力です。" ] || ng ${LINENO}
+
+out=$(echo "1 + a" | ./calculator)
+[ "${out}" = "Input Error: 不明な入力です。" ] || ng ${LINENO}
+
+out=$(echo "1 a 1" | ./calculator)
+[ "${out}" = "Error: その演算子は無効です。 適切な演算子 +, -, *, / を入力してください。" ] || ng ${LINENO}
+
+out=$(echo "1 + 1 + 1" | ./calculator)
+[ "${out}" = "Error: 無効な式です。 適切な式は 1 + 2 のように入力してください。" ] || ng ${LINENO}
+
+out=$(echo "1 / 0" | ./calculator)
+[ "${out}" = "Error: ゼロでの割り算は無効です。" ] || ng ${LINENO}
 
 [ "$ret" = 0 ] && echo OK
 exit $ret
