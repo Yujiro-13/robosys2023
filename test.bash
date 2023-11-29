b2da2449c8696ebe72dfdaf8aed778b0a2906e2c
Yujiro Shito <s.yujiro613@gmal.com>
2023-11-29 09:45:54 +0900

    test.bashの改良

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
-#!/bin/bash -xv 
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yujiro Shito
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
@@ -10,17 +10,31 @@ ng () {
 ret=0
 
 ### I/O TEST ###
+out=$(echo 0 | ./plus)
+[ "${out}" = 0 ] || ng ${LINENO}
+
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+out=$(seq -5 | ./plus)
+[ "${out}" = "Error" ] || ng ${LINENO}
+
 ### STRANGE INPUT ###
+out=$(echo a | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "Error" ] || ng ${LINENO}
+
+out=$(echo A | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "Error" ] || ng ${LINENO}
+
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "${out}" = "Error" ] || ng ${LINENO}
 
 out=$(echo | ./plus) #空文字
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "${out}" = "Error" ] || ng ${LINENO}
 
 [ "$ret" = 0 ] && echo OK
 exit $ret
